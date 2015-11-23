8e3ca11bccfa1dfacb75080938162d937922313f
George Broughton <broughton_g182@hotmail.com>
2015-11-23 15:25:38 +0000

    Create install.sh
    Install script (UNFINISHED)
    
    Adds current user to dialout group.
    
    Creates udev rules for persistently mapping rfid device to /dev/rfid
    
    Doing so allows ROS node to communicate with the device without running as sudo

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+echo "Adding User to correct groups"
+sudo usermod -a -G dialout $USER
+
+read -p "Make sure the RFID reader is unplugged and then press enter" dummy
+unplugged="$(ls /dev | grep "ttyUSB")"
+read -p "Now plug the RFID reader in and then press enter" dummy
+plugged="$(ls /dev | grep "ttyUSB")"
+
+fileList="$(diff <(echo "$unplugged") <(echo "$plugged"))"
+file="$(echo $fileList | cut -d " " -f 5)"
+echo Current location: $file
+echo Persistant symlink: /dev/rfid
+
+prod="$(udevadm info -a -n /dev/$file | grep '{idProduct}' | head -n1 | tr -d '[[:space:]]')"
+vend="$(udevadm info -a -n /dev/$file | grep '{idVendor}' | head -n1 | tr -d '[[:space:]]')"
+seri="$(udevadm info -a -n /dev/$file | grep '{serial}' | head -n1 | tr -d '[[:space:]]')"
+
+echo "SUBSYSTEM==\"tty\", $vend, $prod, $seri, SYMLINK+=\"rfid\"" > /etc/udev/rules.d/99-virtual-usb.rules
+
+sudo udevadm control --reload-rules
+
+echo Settings updated, unplug and replug rfid device for /dev/rfid to appear
