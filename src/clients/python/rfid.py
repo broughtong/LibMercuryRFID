1f841b5aa83d75db79f1e78fa9b80ebace7bd5e5
Manuel <mfernandezcarmona@lincoln.ac.uk>
2016-04-22 10:43:32 +0100

    Reformated python files (no tabs, 4 spaces) rfid.py:    added restart method Inventory.msg: changed field names and types rfid_inventory.py:  now stops reader and decreases power after successful readings

--- src/clients/python/rfid.py
+++ src/clients/python/rfid.py
@@ -11,92 +11,93 @@ callbackList = ""
 callbackHandler = ""
 
 def callbackHandlerFunction(char_ptr):
-	callbackString = ""
+    callbackString = ""
 
-	try:
+    try:
 
-		for i in char_ptr:
-			if i == "\0":
-				break
-			else:
-				callbackString += i
+        for i in char_ptr:
+            if i == "\0":
+                break
+            else:
+                callbackString += i
 
-	except:
-		print "Indexing Error: Pointer to string conversion in wrapper\n"
+    except:
+        print "Indexing Error: Pointer to string conversion in wrapper\n"
 
-	callbackList[0](callbackString)
+    callbackList[0](callbackString)
 
-	return 0
+    return 0
 
 callback = ctypes.CFUNCTYPE(ctypes.c_byte, ctypes.POINTER(ctypes.c_char))(callbackHandlerFunction)
 
 def init():
 
-	global lib, callbackHandler, callbackList
+    global lib, callbackHandler, callbackList
 
-	lib = ctypes.CDLL("libmercuryrfid.so.1")
-	lib.RFIDinit(callback)
+    lib = ctypes.CDLL("libmercuryrfid.so.1")
+    lib.RFIDinit(callback)
 
-	callbackList = {}
+    callbackList = {}
 
 def getHopTime(readerID):
 
-	global lib
+    global lib
 
-	time = lib.getHopTime(readerID)
+    time = lib.getHopTime(readerID)
 
-	return time
+    return time
 
 def setHopTime(readerID, newHopTime):
 
-	global lib
+    global lib
 
-	lib.setHopTime(readerID, newHopTime)
+    lib.setHopTime(readerID, newHopTime)
 
 def startReader(deviceURI, callbackFunction):
 
-	global lib, callbackList, callbackHandler
+    global lib, callbackList, callbackHandler
 
-	readerID = lib.startReader(deviceURI)
+    readerID = lib.startReader(deviceURI)
 
-	#lib.RFIDclose()
-	#lib.getHopTime(readerID)
-	#lib.setHopTime(readerID, 100)
+    #lib.RFIDclose()
+    #lib.getHopTime(readerID)
+    #lib.setHopTime(readerID, 100)
 
-	callbackList[readerID] = callbackFunction
-	return readerID
+    callbackList[readerID] = callbackFunction
+    return readerID
 
 def stopReader(readerID):
 
-	global lib
+    global lib
 
-	lib.stopReader(readerID)
+    lib.stopReader(readerID)
 
-# configures transmision power to value, given in 100*dBm (should be between 500 and 3000)
+# configures READING transmision power to value, given in 100*dBm (should be between 500 and 3000)
+# TODO find out minimum step of power values
 def setPower(readerID, value):
 
-	global lib
+    global lib
 
-	lib.setPower(readerID, value)
+    lib.setPower(readerID, value)
 
 
 def close():
 
-	global lib, callbackList
+    global lib, callbackList
 
-	#lib.closeRFID()
-	lib.RFIDclose()
-	callbackList = {}
+    #lib.closeRFID()
+    lib.RFIDclose()
+    callbackList = {}
 
 # prints on screen current power configuration in 100*dBm
 # TODO return power value
 def getPower(readerID):
 
-        global lib
+    global lib
 
-        power = lib.getPower(readerID)
+    power = lib.getPower(readerID)
 
-        return power
+    return power
 
 
 # newEPCdata is a string with hex char values of epc data
@@ -104,8 +105,16 @@ def getPower(readerID):
 #                  It's half of the length of the string with values!!
 def writeTag(readerId, newEpcData,epcMemoryBytes):
 
-	global lib
+    global lib
 
-	error=lib.writeTag(readerId, newEpcData,epcMemoryBytes)
-        return error
+    error=lib.writeTag(readerId, newEpcData,epcMemoryBytes)
+    return error
+
+# reenables reader reading
+def reStartReader(readerID):
+
+    global lib
+
+    error=lib.reStartReader(readerID)
+    return error
 
--- src/clients/ros/scripts/rfid_inventory.py
+++ src/clients/ros/scripts/rfid_inventory.py
@@ -8,6 +8,7 @@
 
 import roslib
 import rospy
+import rospy.rostime
 import sys
 import time
 import rfid as m6e
@@ -18,57 +19,64 @@ from rfid_node.msg import TagData
 from rfid_node.msg import TagStats
 
 def rfidCallback(message):
-	global inventory_msg
-	global tag_dict
+    global inventory_msg
+    global tag_dict
 
-	inventory_msg.endTime=rospy.get_rostime()
+    inventory_msg.lastTagTime=rospy.get_rostime()
 
-	fields = str(message).split(':')
-	tagID = fields[1]
+    fields = str(message).split(':')
+    tagID = fields[1]
 
-	#get timestamp from fields
-	timestampHigh =int(fields[5])
+    #get timestamp from fields
+    timestampHigh =int(fields[5])
 
-	# solve error with lower bit timestamp containing an i
-	timestampLowStr = fields[6]
-	if timestampLowStr.endswith("i"):
-	    timestampLowStr=timestampLowStr[1:len(timestampLowStr)-1]
+    # solve error with lower bit timestamp containing an i
+    timestampLowStr = fields[6]
+    if timestampLowStr.endswith("i"):
+        timestampLowStr=timestampLowStr[1:len(timestampLowStr)-1]
 
-	timestampLow  =int(timestampLowStr)
-	# timestamp in milisecs
-	timest= ( timestampHigh<<32) | timestampLow
+    timestampLow  =int(timestampLowStr)
+    # timestamp in milisecs
+    timest= ( timestampHigh<<32) | timestampLow
 
-	# a Tag Stats message contains instantaneous transmission data
-	tagSt = TagStats()
-	tagSt.timestamp.secs =  timest/1000
-	tagSt.timestamp.nsecs =  (timest%1000)*1000000
-	tagSt.rssi      = int(fields[2])
-	tagSt.phase     = int(fields[3])
-	tagSt.frequency = int(fields[4])
+    # a Tag Stats message contains instantaneous transmission data
+    tagSt = TagStats()
+    tagSt.timestamp.secs =  timest/1000
+    tagSt.timestamp.nsecs =  (timest%1000)*1000000
+    tagSt.rssi      = int(fields[2])
+    tagSt.phase     = int(fields[3])
+    tagSt.frequency = int(fields[4])
         
-	if tagID not in tag_dict:
-	    newTag = TagData()
-	    newTag.ID = tagID
-	    tag_dict[tagID]=newTag
+    if tagID not in tag_dict:
+        newTag = TagData()
+        newTag.ID = tagID
+        tag_dict[tagID]=newTag
 
-	tag_dict[tagID].stats.append(tagSt)
+    tag_dict[tagID].stats.append(tagSt)
         
-        # print tag data
-        if 0:
-          rospy.loginfo('Detected tag id: %s', tagID)
-          rospy.loginfo('RSSi = %s', fields[2])
-          rospy.loginfo('phase = %s', fields[3])
-          rospy.loginfo('frequency = %s', fields[4])
-          rospy.loginfo('Timestamp = %d', timest)
+    # print tag data
+    if 0:
+      rospy.loginfo('Detected tag id: %s', tagID)
+      rospy.loginfo('RSSi = %s', fields[2])
+      rospy.loginfo('phase = %s', fields[3])
+      rospy.loginfo('frequency = %s', fields[4])
+      rospy.loginfo('Timestamp = %d', timest)
 
 
 # Node example class.
 class RFID_Inventory():
-	
+
     # Must have __init__(self) function for a class, similar to a C++ class constructor.
     def __init__(self):
         global inventory_msg
         global tag_dict
+        global reader
+
+        # "Constants" or variables you should really not change
+        # power is in 100 dBm
+        POWER_STEP=500  # decreasing power after each inventory
+        MIN_POWER=500   # minimum reading power
+        MAX_POWER=3000  # maximum reading power
 
         # Init our global vars...
         inventory_msg = Inventory()
@@ -77,7 +85,7 @@ class RFID_Inventory():
         # Get the ~private namespace parameters from command line or launch file.
         self.tinventory = float(rospy.get_param('~tinventory', '2.0'))
         self.trest = float(rospy.get_param('~trest', '1.0'))
-        self.txpower = int(rospy.get_param('~txpower', '2000'))
+        self.txpower = int(rospy.get_param('~txpower', '3000'))
         self.topic = rospy.get_param('~topic', 'Inventory')
 
         rospy.loginfo('tinventory = %d', self.tinventory)
@@ -85,14 +93,12 @@ class RFID_Inventory():
         rospy.loginfo('trest = %d', self.trest)       
         rospy.loginfo('topic = %s', self.topic)
 
-	# Configure reader and launch
-	m6e.init()
+        # Init reader
+        m6e.init()
         reader = m6e.startReader("tmr:///dev/rfid", rfidCallback)
         m6e.setHopTime(reader, 40)
-	m6e.getPower(reader)        
-        m6e.setPower(reader,self.txpower)
-	m6e.getPower(reader)
-
+        m6e.getPower(reader)
+        m6e.stopReader(reader)
         # Create a publisher
         pub = rospy.Publisher(self.topic, Inventory,queue_size=10)
 
@@ -103,18 +109,29 @@ class RFID_Inventory():
         # Main while loop.
         while not rospy.is_shutdown():
             # Prepare custom mesage
-	    #inventory_msg = Inventory()
-            inventory_msg.startTime = rospy.get_rostime()
-            inventory_msg.maxTimeMiliSecs = int(self.tinventory)  # shall change msg to be a double...
+            #inventory_msg = Inventory()
+            inventory_msg.readingTime = rospy.Duration(self.tinventory)
             inventory_msg.TxPower = self.txpower
             inventory_msg.tagList = []
             tag_dict = {}
-            # get inventory: read tags for specified time
-            self.getInventory()
-            inventory_msg.tagList = tag_dict.values()
 
-            # publish them
-            pub.publish(inventory_msg)
+            m6e.setPower(reader, self.txpower)
+            m6e.getPower(reader)
+
+            # get inventory: read tags for specified time
+            inventory_msg.startTime = rospy.get_rostime() # closest to the real starting moment
+            m6e.reStartReader(reader)
+            self.getInventory()
+            m6e.stopReader(reader)
+
+            # if inventory was successful, do a 'smaller' one
+            if (tag_dict.keys() > 0):
+                inventory_msg.tagList = tag_dict.values()
+                # publish them
+                pub.publish(inventory_msg)
+                self.txpower = self.txpower-POWER_STEP
+                if (self.txpower<MIN_POWER):
+                    self.txpower=MAX_POWER
 
             # Sleep for a while after publishing new messages
             rospy.sleep(self.trest)
@@ -122,14 +139,15 @@ class RFID_Inventory():
 
 
     def getInventory(self):
-	global tag_dict
-        rospy.loginfo('Starting inventory.............................' )
-        # reader will be detecting tags while this is sleeping... 
-        rospy.sleep(self.tinventory)
-	rospy.loginfo('Detected %d different tags:', len(tag_dict))
-	rospy.loginfo('%s', tag_dict.keys())
-        rospy.loginfo('Inventory ended   .............................' )
+        global tag_dict
+        global reader
 
+        rospy.loginfo('Starting inventory.............................' )
+        # reader will be detecting tags while this is sleeping...
+        rospy.sleep(self.tinventory)
+        rospy.loginfo('Detected %d different tags:', len(tag_dict))
+        rospy.loginfo('%s', tag_dict.keys())
+        rospy.loginfo('Inventory ended   .............................' )
 
 
 # Main function.
