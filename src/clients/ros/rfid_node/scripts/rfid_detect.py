627fd6dadf5845b07d9913fdd5663f8ae054a038
George Broughton <broughton_g182@hotmail.com>
2016-06-27 15:32:01 +0100

    Fixed bug with python mem scope

--- src/clients/ros/rfid_node/scripts/rfid_detect.py
+++ src/clients/ros/rfid_node/scripts/rfid_detect.py
@@ -7,23 +7,24 @@ import rfid
 pub = ""
 
 def rfidCallback(message):
-	msg = str(message[0]) + ":" + str(message[1]) + ":" + str(message[2]) + ":" + str(message[3]) + ":"
+
+	#msg = str(message[0]) + ":" + str(message[1]) + ":" + str(message[2]) + ":" + str(message[3]) + ":"
 	# + ":" + str(message[4]) + ":" + str(message[5])
-	print str(msg)
+	#print str(msg)
 	
-	pub.publish(str(msg))
+	#pub.publish(str(msg))
 
 if __name__ == "__main__":
 
-	rospy.init_node("rfid_detect")
-	pub = rospy.Publisher("rfid/rfid_detect", std_msgs.msg.String, queue_size=100)
+	rospy.init_node("rfid_detect1")
+	pub = rospy.Publisher("rfid/rfid_detect1", std_msgs.msg.String, queue_size=100)
 
 	rfid.init()
 	reader = rfid.startReader("tmr:///dev/rfid", rfidCallback)
 
-	print rfid.getHopTime(reader) #defaults to 375
-	rfid.setHopTime(reader, 40) 
-	print rfid.getHopTime(reader)
+	#print rfid.getHopTime(reader) #defaults to 375
+	#rfid.setHopTime(reader, 40) 
+	#print rfid.getHopTime(reader)
 
 	rospy.spin()
 
